5b7ae251aefb830b4c3e73e860de20647dad954f
Juan Manuel Corigliano Giuliani <126042007+juancorax@users.noreply.github.com>
2024-11-18 21:38:30 -0300

    refactor: move flickr gem require statement to the top

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,8 +1,8 @@
+require "flickr"
+
 class StaticPagesController < ApplicationController
   def index
     if params[:flickr_user_id].present?
-      require "flickr"
-
       flickr = Flickr.new
 
       @photos = flickr.photos.search(user_id: params[:flickr_user_id])
